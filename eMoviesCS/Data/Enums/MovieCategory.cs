3a47e8da05bd251bd31811b03542bae709bfa451
Thallstorm <geo.andris@gmail.com>
2024-04-09 01:58:04 +0300

    Add project files.

---
+++ eMoviesCS/Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace eMoviesCS.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Horror,
+        Cartoon
+    }
+}
---
+++ eMoviesCS/Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eMoviesCS.Models
+{
+	public class Actor
+	{
+		[Key]
+		public int Id { get; set; }
+		public string? ProfilePictureURL { get; set; }
+        public required string FullName { get; set; }
+		public string? Bio { get; set; }
+
+		//Relationships
+		public List<Actor_Movie>? Actors_Movies { get; set; }
+
+	}
+}
